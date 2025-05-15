997754b73a15f4342336ef7ae598aaf8bc6d3282
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2025-05-15 15:53:55 +0200

    add oapi generator

--- config/config.exs
+++ config/config.exs
@@ -52,6 +52,12 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :oapi_generator, default: [
+  output: [
+    base_module: Brightsky
+  ]
+]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Wetterhaecker.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:oapi_generator, "~> 0.2.0", only: :dev, runtime: false}
     ]
   end
 
