a2e302c5b7a7ae7d41c53c6c70452d099e7f883c
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2025-06-25 14:50:21 +0200

    (wip) add quokka

--- mix.exs
+++ mix.exs
@@ -74,7 +74,8 @@ defmodule Wetterhaecker.MixProject do
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:ex_check, "~> 0.16.0", only: [:dev], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:quokka, "~> 2.7", only: [:dev, :test], runtime: false}
     ]
   end
 
